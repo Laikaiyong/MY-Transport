e9c60d26ffd6cbfdef03adc3fd329b00e5bfbad5
Laikaiyong <vandycklai@gmail.com>
2024-02-29 15:05:19 +0800

    Import error

--- pages/2_🚍_Bus.py
+++ pages/2_🚍_Bus.py
@@ -2,6 +2,7 @@ from google.transit import gtfs_realtime_pb2
 from google.protobuf.json_format import MessageToDict
 import pandas as pd
 from requests import get
+import random
 
 import streamlit as st
 
